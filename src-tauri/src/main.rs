f83416b095b7b905ebb65b21fabd29b5fe56566d
DasCleverle <dascleverle@gmail.com>
2023-08-19 23:40:13 +0200

    Send chess board to client

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,53 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod chess;
+
+use std::error::Error;
+
+use serde::Serialize;
+use tauri::{App, Wry, Manager, Window, PageLoadPayload};
+
+use crate::chess::{Board, Piece};
+
+#[derive(Serialize, Clone)]
+struct BoardPayload {
+    pieces: Vec<Option<Piece>>
+}
+
+impl From<Board> for BoardPayload {
+    fn from(value: Board) -> Self {
+        let pieces = Vec::from(value.pieces);
+        return BoardPayload { pieces };
+    }
+}
+
+fn init(app: &mut App<Wry>) -> Result<(), Box<dyn Error>> {
+    return Ok(());
+}
+
+fn handle_page_load(window: Window<Wry>, _payload: PageLoadPayload) {
+    let board: Board = Board::new_game();
+    let payload: BoardPayload = board.into();
+
+    println!("emitting board-update");
+    window.emit_all("board-update", payload).expect("emit to be successful");
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_board() -> BoardPayload {
+    println!("getting board");
+    let board: Board = Board::new_game();
+    let payload: BoardPayload = board.into();
+
+    return payload;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(init)
+        .invoke_handler(tauri::generate_handler![get_board])
+        .on_page_load(handle_page_load)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,77 @@
-import { invoke } from "@tauri-apps/api/tauri";
+import type { Event } from '@tauri-apps/api/event';
+import { listen } from "@tauri-apps/api/event";
+import { invoke } from '@tauri-apps/api/tauri';
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+type Piece =
+    | 'BlackRook'
+    | 'BlackKnight'
+    | 'BlackBishop'
+    | 'BlackQueen'
+    | 'BlackKing'
+    | 'BlackPawn'
+    | 'WhiteRook'
+    | 'WhiteKnight'
+    | 'WhiteBishop'
+    | 'WhiteQueen'
+    | 'WhiteKing'
+    | 'WhitePawn';
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+interface BoardPayload {
+    pieces: Piece[];
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+function toCoord(offset: number) {
+    const rank = offset % 8 + 1;
+    const file = String.fromCharCode(97 + Math.floor(offset / 8));
+
+    return `${file}${rank}`;
+}
+
+function getImageName(piece: Piece) {
+    let str = '';
+
+    for (let i = 0; i < piece.length; i++) {
+        const c = piece.charAt(i);
+
+        if (i !== 0 && c == c.toUpperCase()) {
+            str += '_' + c.toLowerCase();
+        }
+        else {
+            str += c;
+        }
+    }
+
+    return str;
+}
+
+function updateBoard(board: BoardPayload) {
+    const pieces = board.pieces;
+
+    for (let i = 0; i < pieces.length; i++) {
+        const piece = pieces[i];
+        const coord = toCoord(i);
+        const el = document.getElementById(coord);
+
+        if (!el) {
+            continue;
+        }
+
+        if (piece) {
+            el.innerHTML = `<img src="/pieces/${getImageName(piece)}.png" />`;
+        }
+        else {
+            el.innerHTML = '';
+        }
+    }
+}
+
+
+// await listen('board-update', (event: Event<BoardPayload>) => {
+//     updateBoard(event.payload);
+//});
+
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log('getting board');
+    const board: BoardPayload = await invoke('get_board');
+    updateBoard(board);
 });
