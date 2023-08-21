962ac823506042595e8109cf8b4e1b74e14fa6df
DasCleverle <dascleverle@gmail.com>
2023-08-22 00:51:52 +0200

    Add moves for all pieces except knight

--- src-tauri/src/chess.rs
+++ src-tauri/src/chess.rs
@@ -1,6 +1,5 @@
-use std::fmt::Display;
-
 use serde::{de::Visitor, Deserialize, Deserializer, Serialize};
+use std::fmt::Display;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Coord {
@@ -9,7 +8,7 @@ pub struct Coord {
 }
 
 fn is_valid_coord(row: u8, column: char) -> bool {
-    row <= 8 && column as u8 <= b'h'
+    row >= 1 && row <= 8 && column as u8 >= b'a' && column as u8 <= b'h'
 }
 
 impl Coord {
@@ -44,15 +43,15 @@ impl Coord {
         ((self.column as u8 - b'a') * 8 + self.row - 1) as usize
     }
 
-    pub fn translate(&self, x: u8, y: u8) -> Option<Coord> {
-        let column: char = (self.column as u8 + x) as char;
-        let row = self.row + y;
-
-        println!("translating {self} to {column}{row}");
+    pub fn translate(&self, x: i8, y: i8) -> Option<Coord> {
+        let column: char = (self.column as i8 + x) as u8 as char;
+        let row = ((self.row as i8) + y) as u8;
 
         if !is_valid_coord(row, column) {
             return None;
         }
+
+        println!("translating {self} to {column}{row}");
 
         return Some(Coord { row, column });
     }
@@ -105,20 +104,44 @@ impl<'de> Deserialize<'de> for Coord {
     }
 }
 
-#[derive(Debug, Copy, Clone, Serialize)]
+#[derive(Debug, Copy, Clone, Serialize, strum_macros::IntoStaticStr, strum_macros::Display)]
+pub enum Color {
+    White,
+    Black,
+}
+
+#[derive(Debug, Copy, Clone, strum_macros::IntoStaticStr)]
 pub enum Piece {
-    BlackRook,
-    BlackKnight,
-    BlackBishop,
-    BlackQueen,
-    BlackKing,
-    BlackPawn,
-    WhiteRook,
-    WhiteKnight,
-    WhiteBishop,
-    WhiteQueen,
-    WhiteKing,
-    WhitePawn,
+    Rook(Color),
+    Knight(Color),
+    Bishop(Color),
+    Queen(Color),
+    King(Color),
+    Pawn(Color),
+}
+
+impl Display for Piece {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Piece::Rook(c) => f.write_str(c.into())?,
+            Piece::Knight(c) => f.write_str(c.into())?,
+            Piece::Bishop(c) => f.write_str(c.into())?,
+            Piece::Queen(c) => f.write_str(c.into())?,
+            Piece::King(c) => f.write_str(c.into())?,
+            Piece::Pawn(c) => f.write_str(c.into())?,
+        }
+
+        f.write_str(self.into())
+    }
+}
+
+impl Serialize for Piece {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.collect_str(self)
+    }
 }
 
 #[derive(Copy, Clone, Serialize, Deserialize)]
@@ -135,41 +158,41 @@ impl Board {
     pub fn new_game() -> Board {
         let mut board = Board { pieces: [None; 64] };
 
-        board.set(Coord::new('a', 1), Piece::WhiteRook);
-        board.set(Coord::new('b', 1), Piece::WhiteKnight);
-        board.set(Coord::new('c', 1), Piece::WhiteBishop);
-        board.set(Coord::new('d', 1), Piece::WhiteQueen);
-        board.set(Coord::new('e', 1), Piece::WhiteKing);
-        board.set(Coord::new('f', 1), Piece::WhiteBishop);
-        board.set(Coord::new('g', 1), Piece::WhiteKnight);
-        board.set(Coord::new('h', 1), Piece::WhiteRook);
+        board.set(Coord::new('a', 1), Piece::Rook(Color::White));
+        board.set(Coord::new('b', 1), Piece::Knight(Color::White));
+        board.set(Coord::new('c', 1), Piece::Bishop(Color::White));
+        board.set(Coord::new('d', 1), Piece::Queen(Color::White));
+        board.set(Coord::new('e', 1), Piece::King(Color::White));
+        board.set(Coord::new('f', 1), Piece::Bishop(Color::White));
+        board.set(Coord::new('g', 1), Piece::Knight(Color::White));
+        board.set(Coord::new('h', 1), Piece::Rook(Color::White));
 
-        board.set(Coord::new('a', 2), Piece::WhitePawn);
-        board.set(Coord::new('b', 2), Piece::WhitePawn);
-        board.set(Coord::new('c', 2), Piece::WhitePawn);
-        board.set(Coord::new('d', 2), Piece::WhitePawn);
-        board.set(Coord::new('e', 2), Piece::WhitePawn);
-        board.set(Coord::new('f', 2), Piece::WhitePawn);
-        board.set(Coord::new('g', 2), Piece::WhitePawn);
-        board.set(Coord::new('h', 2), Piece::WhitePawn);
+        board.set(Coord::new('a', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('b', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('c', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('d', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('e', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('f', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('g', 2), Piece::Pawn(Color::White));
+        board.set(Coord::new('h', 2), Piece::Pawn(Color::White));
 
-        board.set(Coord::new('a', 7), Piece::BlackPawn);
-        board.set(Coord::new('b', 7), Piece::BlackPawn);
-        board.set(Coord::new('c', 7), Piece::BlackPawn);
-        board.set(Coord::new('d', 7), Piece::BlackPawn);
-        board.set(Coord::new('e', 7), Piece::BlackPawn);
-        board.set(Coord::new('f', 7), Piece::BlackPawn);
-        board.set(Coord::new('g', 7), Piece::BlackPawn);
-        board.set(Coord::new('h', 7), Piece::BlackPawn);
+        board.set(Coord::new('a', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('b', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('c', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('d', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('e', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('f', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('g', 7), Piece::Pawn(Color::Black));
+        board.set(Coord::new('h', 7), Piece::Pawn(Color::Black));
 
-        board.set(Coord::new('a', 8), Piece::BlackRook);
-        board.set(Coord::new('b', 8), Piece::BlackKnight);
-        board.set(Coord::new('c', 8), Piece::BlackBishop);
-        board.set(Coord::new('d', 8), Piece::BlackQueen);
-        board.set(Coord::new('e', 8), Piece::BlackKing);
-        board.set(Coord::new('f', 8), Piece::BlackBishop);
-        board.set(Coord::new('g', 8), Piece::BlackKnight);
-        board.set(Coord::new('h', 8), Piece::BlackRook);
+        board.set(Coord::new('a', 8), Piece::Rook(Color::Black));
+        board.set(Coord::new('b', 8), Piece::Knight(Color::Black));
+        board.set(Coord::new('c', 8), Piece::Bishop(Color::Black));
+        board.set(Coord::new('d', 8), Piece::Queen(Color::Black));
+        board.set(Coord::new('e', 8), Piece::King(Color::Black));
+        board.set(Coord::new('f', 8), Piece::Bishop(Color::Black));
+        board.set(Coord::new('g', 8), Piece::Knight(Color::Black));
+        board.set(Coord::new('h', 8), Piece::Rook(Color::Black));
 
         return board;
     }
@@ -179,10 +202,13 @@ impl Board {
     }
 
     fn peek(&self, coord: Coord) -> (Option<Piece>, Coord) {
-        return (self.pieces[coord.to_offset()], coord);
+        let result = (self.pieces[coord.to_offset()], coord);
+        dbg!(result);
+
+        return result;
     }
 
-    fn peek_translated(&self, coord: Coord, x: u8, y: u8) -> Option<(Option<Piece>, Coord)> {
+    fn peek_translated(&self, coord: Coord, x: i8, y: i8) -> Option<(Option<Piece>, Coord)> {
         return coord.translate(x, y).map(|c| (self.peek(c).0, c));
     }
 
@@ -190,38 +216,115 @@ impl Board {
         return self.pieces;
     }
 
+    fn walk<X, Y>(&self, moves: &mut Vec<Move>, from: Coord, get_x: X, get_y: Y) -> ()
+    where
+        X: Fn(i8) -> Option<i8>,
+        Y: Fn(i8) -> Option<i8>,
+    {
+        let mut x_counter = 0;
+        let mut y_counter = 0;
+
+        loop {
+            match (get_x(x_counter), get_y(y_counter)) {
+                (Some(x), Some(y)) => {
+                    if let Some((other_piece, to)) = self.peek_translated(from, x, y) {
+                        if other_piece.is_none() {
+                            moves.push(Move { from, to })
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+
+                    x_counter = x;
+                    y_counter = y;
+                },
+                _ => break,
+            }
+
+        }
+    }
+
     pub fn get_available_moves(&self, from: Coord) -> Option<Vec<Move>> {
         return self.peek(from).0.map(|piece| {
-            println!("found {piece:?} at {from}");
+            let mut moves: Vec<Move> = Vec::new();
 
             match piece {
-                Piece::WhitePawn => {
-                    let mut moves: Vec<Move> = Vec::new();
-
+                Piece::Pawn(Color::White) => {
                     if let Some((other_piece, to)) = self.peek_translated(from, 0, 1) {
                         if other_piece.is_none() {
-                            moves.push(Move {
-                                from,
-                                to,
-                            });
+                            moves.push(Move { from, to });
+                        } else {
+                            return moves;
                         }
                     }
 
                     if from.row == 2 {
                         if let Some((other_piece, to)) = self.peek_translated(from, 0, 2) {
                             if other_piece.is_none() {
-                                moves.push(Move {
-                                    from,
-                                    to,
-                                });
+                                moves.push(Move { from, to });
                             }
                         }
                     }
-
-                    return moves;
                 }
-                _ => Vec::new(),
-            }
+                Piece::Pawn(Color::Black) => {
+                    if let Some((other_piece, to)) = self.peek_translated(from, 0, -1) {
+                        if other_piece.is_none() {
+                            moves.push(Move { from, to });
+                        } else {
+                            return moves;
+                        }
+                    }
+
+                    if from.row == 7 {
+                        if let Some((other_piece, to)) = self.peek_translated(from, 0, -2) {
+                            if other_piece.is_none() {
+                                moves.push(Move { from, to });
+                            }
+                        }
+                    }
+                }
+                Piece::Rook(_) => {
+                    self.walk(&mut moves, from, |x| Some(x + 1), |_| Some(0));
+                    self.walk(&mut moves, from, |x| Some(x - 1), |_| Some(0));
+                    self.walk(&mut moves, from, |_| Some(0), |y| Some(y + 1));
+                    self.walk(&mut moves, from, |_| Some(0), |y| Some(y - 1));
+                },
+                Piece::Bishop(_) => {
+                    self.walk(&mut moves, from, |x| Some(x + 1), |y| Some(y + 1));
+                    self.walk(&mut moves, from, |x| Some(x - 1), |y| Some(y - 1));
+                    self.walk(&mut moves, from, |x| Some(x + 1), |y| Some(y - 1));
+                    self.walk(&mut moves, from, |x| Some(x - 1), |y| Some(y + 1));
+                },
+                Piece::Queen(_) => {
+                    self.walk(&mut moves, from, |x| Some(x + 1), |_| Some(0));
+                    self.walk(&mut moves, from, |x| Some(x - 1), |_| Some(0));
+                    self.walk(&mut moves, from, |_| Some(0), |y| Some(y + 1));
+                    self.walk(&mut moves, from, |_| Some(0), |y| Some(y - 1));
+                    self.walk(&mut moves, from, |x| Some(x + 1), |y| Some(y + 1));
+                    self.walk(&mut moves, from, |x| Some(x - 1), |y| Some(y - 1));
+                    self.walk(&mut moves, from, |x| Some(x + 1), |y| Some(y - 1));
+                    self.walk(&mut moves, from, |x| Some(x - 1), |y| Some(y + 1));
+                },
+                Piece::Knight(_) => {
+
+                },
+                Piece::King(_) => {
+                    self.walk(&mut moves, from, |x| if x >= 1 { None } else { Some(x + 1) }, |_| Some(0));
+                    self.walk(&mut moves, from, |x| if x <= -1 { None } else { Some(x - 1) }, |_| Some(0));
+                    self.walk(&mut moves, from, |_| Some(0), |y| if y >= 1 { None } else { Some(y + 1) });
+                    self.walk(&mut moves, from, |_| Some(0), |y| if y <= -1 { None } else { Some(y - 1) });
+                    self.walk(&mut moves, from, |x| if x >= 1 { None } else { Some(x + 1) }, |y| if y >= 1 { None } else { Some(y + 1) });
+                    self.walk(&mut moves, from, |x| if x <= -1 { None } else { Some(x - 1) }, |y| if y <= -1 { None } else { Some(y - 1) });
+                    self.walk(&mut moves, from, |x| if x >= 1 { None } else { Some(x + 1) }, |y| if y <= -1 { None } else { Some(y - 1) });
+                    self.walk(&mut moves, from, |x| if x <= -1 { None } else { Some(x - 1) }, |y| if y >= 1 { None } else { Some(y + 1) });
+                }
+            };
+
+            return moves;
         });
     }
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[macro_use] extern crate serde;
+
 mod chess;
 
 use std::sync::{Arc, Mutex};
