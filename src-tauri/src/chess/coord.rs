8608999ee2f988da060c46614b4c8ced3a4a204b
DasCleverle <dascleverle@gmail.com>
2023-08-27 23:15:08 +0200

    Don't error on invalid coord move as it was too slow

--- src-tauri/src/chess/coord.rs
+++ src-tauri/src/chess/coord.rs
@@ -4,24 +4,26 @@ use anyhow::Result;
 
 use serde::{de::Visitor, Deserialize, Deserializer, Serialize};
 
-const DIRECTION_NORTH: usize = 0;
-const DIRECTION_EAST: usize = 1;
-const DIRECTION_SOUTH: usize = 2;
-const DIRECTION_WEST: usize = 3;
-
 lazy_static! {
     static ref DISTANCES_TO_EDGE: [[isize; 4]; 64] = {
         let mut distances: [[isize; 4]; 64] = [[0; 4]; 64];
 
         for i in 0..64 {
-            distances[i][DIRECTION_NORTH] = 8 - (i as isize / 8);
-            distances[i][DIRECTION_EAST] = 7 - (i as isize % 8);
-            distances[i][DIRECTION_SOUTH] = i as isize / 8;
-            distances[i][DIRECTION_WEST] = i as isize % 8;
+            distances[i][Direction::North as usize] = 7 - (i / 8) as isize;
+            distances[i][Direction::East as usize] = 7 - (i % 8) as isize;
+            distances[i][Direction::South as usize] = (i / 8) as isize;
+            distances[i][Direction::West as usize] = (i % 8) as isize;
         }
 
         return distances;
     };
+}
+
+pub enum Direction {
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Default)]
@@ -74,58 +76,50 @@ impl Coord {
         return row as u8;
     }
 
-    pub fn distance(&self, other: Self) -> (isize, isize) {
-        let x = (self.column() as isize) - (other.column() as isize);
-        let y = (self.row() as isize) - (other.row() as isize);
+    pub fn mv_mut(&mut self, row: isize, column: isize) -> bool {
+        if let Some(offset) = self.get_move_offset(row, column) {
+            self.offset = self.offset as isize + offset;
+            return true;
+        }
 
-        return (x, y);
+        return false;
     }
 
-    fn get_move_offset(&self, row: isize, column: isize) -> Result<isize> {
+    pub fn mv(&self, row: isize, column: isize) -> Option<Self> {
+        let offset = self.get_move_offset(row, column)?;
+        return Some(Coord { offset: self.offset + offset });
+    }
+
+    fn get_move_offset(&self, row: isize, column: isize) -> Option<isize> {
         if row == 0 && column == 0 {
-            return Ok(0);
+            return Some(0);
         }
 
         let mut offset = 0;
 
         if row != 0 {
-            let row_dir = if row < 0 { DIRECTION_WEST } else { DIRECTION_EAST };
+            let row_dir = if row < 0 { Direction::West } else { Direction::East } as usize;
             let row_dist = DISTANCES_TO_EDGE[self.offset()][row_dir];
 
             if row.abs() > row_dist {
-                return Err(if row < 0 { MoveErr::WestEdge } else { MoveErr::EastEdge }.into());
+                return None;
             }
 
             offset += row;
         }
 
         if column != 0 {
-            let col_dir = if column < 0 { DIRECTION_SOUTH } else { DIRECTION_NORTH };
+            let col_dir = if column < 0 { Direction::South } else { Direction::North } as usize;
             let col_dist = DISTANCES_TO_EDGE[self.offset()][col_dir];
 
             if column.abs() > col_dist {
-                return Err(if row < 0 { MoveErr::SouthEdge } else { MoveErr::NorthEdge }.into());
+                return None;
             }
 
             offset += column * 8;
         }
 
-        return Ok(offset);
-    }
-
-    pub fn mv_mut(&mut self, row: isize, column: isize) -> Result<()> {
-        let offset = self.get_move_offset(row, column)?;
-        self.offset = self.offset as isize + offset;
-
-        return Ok(());
-    }
-
-    pub fn mv(&self, row: isize, column: isize) -> Result<Self> {
-        let offset = self.get_move_offset(row, column)?;
-
-        return Ok(Coord {
-            offset: self.offset + offset,
-        });
+        return Some(offset);
     }
 }
 
@@ -171,21 +165,6 @@ impl<'de> Deserialize<'de> for Coord {
     {
         deserializer.deserialize_str(CoordVisitor)
     }
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum MoveErr {
-    #[error("reached east edge of the board")]
-    EastEdge,
-
-    #[error("reached west edge of the board")]
-    WestEdge,
-
-    #[error("reached north edge of the board")]
-    NorthEdge,
-
-    #[error("reached south edge of the board")]
-    SouthEdge,
 }
 
 #[cfg(test)]
@@ -262,42 +241,32 @@ mod tests {
     }
 
     #[test]
-    fn move_south() -> Result<()> {
+    fn move_south() {
         let mut one_south = Coord::new('a', 2);
-        one_south.mv_mut(0, -1)?;
-
+        one_south.mv_mut(0, -1);
         assert_eq!(0, one_south.offset(), "a2 -> a1");
 
         let mut two_south = Coord::new('b', 5);
-        two_south.mv_mut(0, -2)?;
-
+        two_south.mv_mut(0, -2);
         assert_eq!(17, two_south.offset(), "b5 -> b3");
 
         let mut seven_south = Coord::new('h', 8);
-        seven_south.mv_mut(0, -7)?;
-
+        seven_south.mv_mut(0, -7);
         assert_eq!(7, seven_south.offset(), "h8 -> h1");
-
-        return Ok(());
     }
 
     #[test]
-    fn move_east() -> Result<()> {
+    fn move_east() {
         let mut one_east = Coord::new('a', 2);
-        one_east.mv_mut(1, 0)?;
-
+        one_east.mv_mut(1, 0);
         assert_eq!(9, one_east.offset(), "a2 -> b2");
 
         let mut two_east = Coord::new('b', 5);
-        two_east.mv_mut(2, 0)?;
-
+        two_east.mv_mut(2, 0);
         assert_eq!(35, two_east.offset(), "b5 -> d5");
 
         let mut seven_east = Coord::new('a', 4);
-        seven_east.mv_mut(7, 0)?;
-
+        seven_east.mv_mut(7, 0);
         assert_eq!(31, seven_east.offset(), "a4 -> h4");
-
-        return Ok(());
     }
 }
