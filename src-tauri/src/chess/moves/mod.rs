5859147b0788ba60931daf08fdc276c94a3a7bfd
DasCleverle <dascleverle@gmail.com>
2023-08-28 15:36:47 +0200

    Generate all regular pseudo-legal moves for all pieces

--- src-tauri/src/chess/moves/mod.rs
+++ src-tauri/src/chess/moves/mod.rs
@@ -1,15 +1,128 @@
 mod sliding;
 
-use std::{fmt::Display, rc::Rc};
+use std::fmt::Display;
 
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
-use super::{bitboard::BitBoard, Board, Coord};
+use super::{bitboard::BitBoard, board::BoardSide, Board, Color, Coord};
+
+const KNIGHT_JUMPS: [(isize, isize); 8] = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)];
+const KING_MOVES: [(isize, isize); 8] = [(-1, 0), (-1, -1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)];
+
+pub fn get_moves(board: &Board) -> Result<Vec<Move>> {
+    let mut moves: Vec<Move> = Vec::new();
+    let side = board.side(board.turn());
+
+    for rook in side.rooks {
+        into_moves(&mut moves, rook, get_rook_moves(rook, &side, board));
+    }
+
+    for bishop in side.bishops {
+        into_moves(&mut moves, bishop, get_bishop_moves(bishop, &side, board));
+    }
+
+    for queen in side.queens {
+        into_moves(&mut moves, queen, get_queen_moves(queen, &side, board));
+    }
+
+    for pawn in side.pawns {
+        into_moves(&mut moves, pawn, get_pawn_moves(pawn, &side, board));
+    }
+
+    for knight in side.knights {
+        into_moves(&mut moves, knight, get_knight_moves(knight, &side));
+    }
+
+    into_moves(&mut moves, side.king, get_king_moves(side.king, &side));
+
+    return Ok(moves);
+}
+
+fn get_rook_moves(from: Coord, side: &BoardSide, board: &Board) -> BitBoard {
+    sliding::get_rook_move_mask(from, board.all(), &side.all)
+}
+
+fn get_bishop_moves(from: Coord, side: &BoardSide, board: &Board) -> BitBoard {
+    sliding::get_bishop_move_mask(from, board.all(), &side.all)
+}
+
+fn get_queen_moves(from: Coord, side: &BoardSide, board: &Board) -> BitBoard {
+    get_rook_moves(from, side, board) | get_bishop_moves(from, side, board)
+}
+
+fn get_pawn_moves(from: Coord, side: &BoardSide, board: &Board) -> BitBoard {
+    let (move_dir, attack_left, attack_right, start_row) = match board.turn() {
+        Color::White => ((0, 1), (-1, 1), (1, 1), 2),
+        Color::Black => ((0, -1), (-1, -1), (1, -1), 7),
+    };
+
+    let mut pawn_moves = BitBoard::new(0);
+    let opponent_pieces = &board.side(board.turn().invert()).all;
+
+    if let Some(move_coord) = from.mv(move_dir.0, move_dir.1) {
+        pawn_moves.set(move_coord);
+    }
+
+    if let Some(attack_left_coord) = from.mv(attack_left.0, attack_left.1) {
+        if opponent_pieces.is_set(attack_left_coord) {
+            pawn_moves.set(attack_left_coord);
+        }
+    }
+
+    if let Some(attack_right_coord) = from.mv(attack_right.0, attack_right.1) {
+        if opponent_pieces.is_set(attack_right_coord) {
+            pawn_moves.set(attack_right_coord);
+        }
+    }
+
+    if from.row() == start_row {
+        if let Some(move_coord) = from.mv(move_dir.0, move_dir.1 * 2) {
+            pawn_moves.set(move_coord);
+        }
+    }
+
+    // TODO: en passant
+
+    pawn_moves = pawn_moves & !side.all;
+
+    return pawn_moves;
+}
+
+fn get_knight_moves(from: Coord, side: &BoardSide) -> BitBoard {
+    let mut knight_moves = BitBoard::new(0);
+
+    for jump in &KNIGHT_JUMPS {
+        if let Some(target) = from.mv(jump.0, jump.1) {
+            knight_moves.set(target);
+        }
+    }
+
+    knight_moves = knight_moves & !side.all;
+
+    return knight_moves;
+}
+
+fn get_king_moves(from: Coord, side: &BoardSide) -> BitBoard {
+    let mut king_moves = BitBoard::new(0);
+
+    for direction in &KING_MOVES {
+        if let Some(target) = from.mv(direction.0, direction.1) {
+            king_moves.set(target);
+        }
+    }
+
+    // TODO: prevent self check
+    // TODO: castling
+
+    king_moves = king_moves & !side.all;
+
+    return king_moves;
+}
 
 fn into_moves(moves: &mut Vec<Move>, from: Coord, board: BitBoard) {
     for coord in board {
-        moves.push(Move::new(from, coord, false));
+        moves.push(Move::new(from, coord));
     }
 }
 
@@ -17,69 +130,11 @@ fn into_moves(moves: &mut Vec<Move>, from: Coord, board: BitBoard) {
 pub struct Move {
     pub from: Coord,
     pub to: Coord,
-    pub castle: Option<Rc<Move>>,
-    pub allows_en_passant: bool,
-    pub en_passant_victim: Option<Coord>,
 }
 
 impl Move {
-    pub fn new(from: Coord, to: Coord, allows_en_passant: bool) -> Self {
-        return Move {
-            from,
-            to,
-            castle: None,
-            allows_en_passant,
-            en_passant_victim: None,
-        };
-    }
-    //
-    // pub fn new_castling(from: Coord, to: Coord, rook_from: Coord, rook_to: Coord) -> Self {
-    //     return Move {
-    //         from,
-    //         to,
-    //         castle: Some(Rc::new(Move::new(rook_from, rook_to, false))),
-    //         allows_en_passant: false,
-    //         en_passant_victim: None,
-    //     };
-    // }
-    //
-    // pub fn new_en_passant(from: Coord, to: Coord, victim: Coord) -> Self {
-    //     return Move {
-    //         from,
-    //         to,
-    //         castle: None,
-    //         allows_en_passant: false,
-    //         en_passant_victim: Some(victim),
-    //     };
-    // }
-    //
-    //
-
-    pub fn get_moves(board: &Board) -> Result<Vec<Move>> {
-        let mut moves: Vec<Move> = Vec::new();
-        let side = board.side(board.turn());
-
-        let all_pieces = board.all();
-        let friendly_pieces = &side.all;
-
-        for rook in side.rooks {
-            let mask = sliding::get_rook_move_mask(rook, all_pieces, friendly_pieces);
-            into_moves(&mut moves, rook, mask);
-        }
-
-        for bishop in side.bishops {
-            let mask = sliding::get_bishop_move_mask(bishop, all_pieces, friendly_pieces);
-            into_moves(&mut moves, bishop, mask);
-        }
-
-        for queen in side.queens {
-            let rook_moves = sliding::get_rook_move_mask(queen, all_pieces, friendly_pieces);
-            let bishop_moves = sliding::get_bishop_move_mask(queen, all_pieces, friendly_pieces);
-
-            into_moves(&mut moves, queen, rook_moves | bishop_moves);
-        }
-
-        return Ok(moves);
+    pub fn new(from: Coord, to: Coord) -> Self {
+        return Move { from, to };
     }
 }
 
@@ -96,7 +151,11 @@ mod tests {
     #[test]
     fn rook_moves() {
         let board = Board::from_fen("8/2k5/8/7p/8/8/4K3/R6R").unwrap();
-        let moves = Move::get_moves(&board).unwrap();
+        let moves = get_moves(&board)
+            .unwrap()
+            .into_iter()
+            .filter(|mv| mv.from == Coord::new('a', 1) || mv.from == Coord::new('h', 1))
+            .collect::<Vec<Move>>();
 
         assert_eq!(23, moves.len());
     }
@@ -104,11 +163,11 @@ mod tests {
     #[test]
     fn bishop_moves() {
         let board = Board::from_fen("8/2k5/8/B6p/8/5B2/4K3/8").unwrap();
-        let moves = Move::get_moves(&board).unwrap();
-
-        for mv in &moves {
-            println!("{mv}");
-        }
+        let moves = get_moves(&board)
+            .unwrap()
+            .into_iter()
+            .filter(|mv| mv.from == Coord::new('a', 5) || mv.from == Coord::new('f', 3))
+            .collect::<Vec<Move>>();
 
         assert_eq!(15, moves.len());
     }
@@ -116,8 +175,48 @@ mod tests {
     #[test]
     fn queen_moves() {
         let board = Board::from_fen("8/8/1k6/6p1/8/4Q3/3K4/8").unwrap();
-        let moves = Move::get_moves(&board).unwrap();
+        let moves = get_moves(&board)
+            .unwrap()
+            .into_iter()
+            .filter(|mv| mv.from == Coord::new('e', 3))
+            .collect::<Vec<Move>>();
 
         assert_eq!(21, moves.len());
+    }
+
+    #[test]
+    fn pawn_moves() {
+        let board = Board::from_fen("8/8/1k6/6p1/5P2/8/3P4/4K3").unwrap();
+        let moves = get_moves(&board)
+            .unwrap()
+            .into_iter()
+            .filter(|mv| mv.from == Coord::new('d', 2) || mv.from == Coord::new('f', 4))
+            .collect::<Vec<Move>>();
+
+        assert_eq!(4, moves.len());
+    }
+
+    #[test]
+    fn knight_moves() {
+        let board = Board::from_fen("8/8/1k2N3/6p1/5P2/8/3P4/4K3").unwrap();
+        let moves = get_moves(&board)
+            .unwrap()
+            .into_iter()
+            .filter(|mv| mv.from == Coord::new('e', 6))
+            .collect::<Vec<Move>>();
+
+        assert_eq!(7, moves.len());
+    }
+
+    #[test]
+    fn king_moves() {
+        let board = Board::from_fen("8/8/1k2N3/6p1/5P2/8/3P4/4K3").unwrap();
+        let moves = get_moves(&board)
+            .unwrap()
+            .into_iter()
+            .filter(|mv| mv.from == Coord::new('e', 1))
+            .collect::<Vec<Move>>();
+
+        assert_eq!(4, moves.len());
     }
 }
